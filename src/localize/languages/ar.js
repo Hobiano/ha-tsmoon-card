479654a8742d4e92b4c90a1ab65d00ed7e8adf0c
Ihab <ihabmuradtaha@gmail.com>
2025-04-06 08:58:31 +0200

    Update ar.js

--- src/localize/languages/ar.js
+++ src/localize/languages/ar.js
@@ -1,39 +1,31 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 \{\
-  "common": \{\
-    "version": "\uc0\u1575 \u1604 \u1573 \u1589 \u1583 \u1575 \u1585 ",\
-    "title": "\uc0\u1575 \u1604 \u1588 \u1605 \u1587  \u1608 \u1575 \u1604 \u1602 \u1605 \u1585  \u1575 \u1604 \u1576 \u1587 \u1610 \u1591 ",\
-    "title_card": "\uc0\u1576 \u1591 \u1575 \u1602 \u1577  \u1575 \u1604 \u1588 \u1605 \u1587  \u1608 \u1575 \u1604 \u1602 \u1605 \u1585  \u1575 \u1604 \u1576 \u1587 \u1610 \u1591 \u1577 ",\
-    "description": "\uc0\u1576 \u1591 \u1575 \u1602 \u1577  \u1576 \u1587 \u1610 \u1591 \u1577  \u1604 \u1593 \u1585 \u1590  \u1581 \u1575 \u1604 \u1577  \u1575 \u1604 \u1588 \u1605 \u1587  \u1608 \u1575 \u1604 \u1602 \u1605 \u1585 .",\
-    "invalid_configuration": "\uc0\u1578 \u1603 \u1608 \u1610 \u1606  \u1594 \u1610 \u1585  \u1589 \u1575 \u1604 \u1581 "\
-  \},\
-  "editor": \{\
-    "moon_entity": "\uc0\u1593 \u1606 \u1589 \u1585  \u1575 \u1604 \u1602 \u1605 \u1585  (\u1575 \u1582 \u1578 \u1610 \u1575 \u1585 \u1610 )"\
-  \},\
-  "errors": \{\
-    "missing_moon_entity": "\uc0\u1593 \u1606 \u1589 \u1585  \u1575 \u1604 \u1602 \u1605 \u1585  \u1605 \u1601 \u1602 \u1608 \u1583  \u1601 \u1610  \u1575 \u1604 \u1578 \u1603 \u1608 \u1610 \u1606 ",\
-    "invalid_icon_type": "\uc0\u1575 \u1604 \u1571 \u1604 \u1608 \u1575 \u1606  \u1575 \u1604 \u1578 \u1575 \u1604 \u1610 \u1577  \u1601 \u1610  \u1578 \u1603 \u1608 \u1610 \u1606 \u1603  \u1594 \u1610 \u1585  \u1589 \u1575 \u1604 \u1581 \u1577 :"\
-  \},\
-  "card": \{\
-    "moon_phase": "\uc0\u1591 \u1608 \u1585  \u1575 \u1604 \u1602 \u1605 \u1585 ",\
-    "moon_rise": "\uc0\u1588 \u1585 \u1608 \u1602  \u1575 \u1604 \u1602 \u1605 \u1585 ",\
-    "moon_set": "\uc0\u1594 \u1585 \u1608 \u1576  \u1575 \u1604 \u1602 \u1605 \u1585 "\
-  \},\
-  "moon": \{\
-    "new_moon": "\uc0\u1575 \u1604 \u1607 \u1604 \u1575 \u1604  \u1575 \u1604 \u1580 \u1583 \u1610 \u1583 ",\
-    "waxing_crescent": "\uc0\u1607 \u1604 \u1575 \u1604  \u1605 \u1578 \u1586 \u1575 \u1610 \u1583 ",\
-    "first_quarter": "\uc0\u1575 \u1604 \u1585 \u1576 \u1593  \u1575 \u1604 \u1571 \u1608 \u1604 ",\
-    "waxing_gibbous": "\uc0\u1602 \u1605 \u1585  \u1605 \u1578 \u1590 \u1582 \u1605 ",\
-    "full_moon": "\uc0\u1576 \u1583 \u1585 ",\
-    "waning_gibbous": "\uc0\u1602 \u1605 \u1585  \u1605 \u1578 \u1606 \u1575 \u1602 \u1589 ",\
-    "last_quarter": "\uc0\u1575 \u1604 \u1585 \u1576 \u1593  \u1575 \u1604 \u1571 \u1582 \u1610 \u1585 ",\
-    "waning_crescent": "\uc0\u1607 \u1604 \u1575 \u1604  \u1605 \u1578 \u1606 \u1575 \u1602 \u1589 "\
-  \}\
-\}\
+{
+  "common": {
+    "version": "الإصدار",
+    "title": "الشمس والقمر البسيط",
+    "title_card": "بطاقة الشمس والقمر البسيطة",
+    "description": "بطاقة بسيطة لعرض حالة الشمس والقمر.",
+    "invalid_configuration": "تكوين غير صالح"
+  },
+  "editor": {
+    "moon_entity": "عنصر القمر (اختياري)"
+  },
+  "errors": {
+    "missing_moon_entity": "عنصر القمر مفقود في التكوين",
+    "invalid_icon_type": "الألوان التالية في تكوينك غير صالحة:"
+  },
+  "card": {
+    "moon_phase": "طور القمر",
+    "moon_rise": "شروق القمر",
+    "moon_set": "غروب القمر"
+  },
+  "moon": {
+    "new_moon": "الهلال الجديد",
+    "waxing_crescent": "هلال متزايد",
+    "first_quarter": "الربع الأول",
+    "waxing_gibbous": "قمر متضخم",
+    "full_moon": "بدر",
+    "waning_gibbous": "قمر متناقص",
+    "last_quarter": "الربع الأخير",
+    "waning_crescent": "هلال متناقص"
+  }
 }
