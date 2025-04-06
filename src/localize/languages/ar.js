70b60c2f72f501e30f86dfe7f5fb06b632a41c4c
Ihab <ihabmuradtaha@gmail.com>
2025-04-06 08:57:38 +0200

    Add files via upload
    Arabic language

---
+++ src/localize/languages/ar.js
@@ -0,0 +1,39 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 \{\
+  "common": \{\
+    "version": "\uc0\u1575 \u1604 \u1573 \u1589 \u1583 \u1575 \u1585 ",\
+    "title": "\uc0\u1575 \u1604 \u1588 \u1605 \u1587  \u1608 \u1575 \u1604 \u1602 \u1605 \u1585  \u1575 \u1604 \u1576 \u1587 \u1610 \u1591 ",\
+    "title_card": "\uc0\u1576 \u1591 \u1575 \u1602 \u1577  \u1575 \u1604 \u1588 \u1605 \u1587  \u1608 \u1575 \u1604 \u1602 \u1605 \u1585  \u1575 \u1604 \u1576 \u1587 \u1610 \u1591 \u1577 ",\
+    "description": "\uc0\u1576 \u1591 \u1575 \u1602 \u1577  \u1576 \u1587 \u1610 \u1591 \u1577  \u1604 \u1593 \u1585 \u1590  \u1581 \u1575 \u1604 \u1577  \u1575 \u1604 \u1588 \u1605 \u1587  \u1608 \u1575 \u1604 \u1602 \u1605 \u1585 .",\
+    "invalid_configuration": "\uc0\u1578 \u1603 \u1608 \u1610 \u1606  \u1594 \u1610 \u1585  \u1589 \u1575 \u1604 \u1581 "\
+  \},\
+  "editor": \{\
+    "moon_entity": "\uc0\u1593 \u1606 \u1589 \u1585  \u1575 \u1604 \u1602 \u1605 \u1585  (\u1575 \u1582 \u1578 \u1610 \u1575 \u1585 \u1610 )"\
+  \},\
+  "errors": \{\
+    "missing_moon_entity": "\uc0\u1593 \u1606 \u1589 \u1585  \u1575 \u1604 \u1602 \u1605 \u1585  \u1605 \u1601 \u1602 \u1608 \u1583  \u1601 \u1610  \u1575 \u1604 \u1578 \u1603 \u1608 \u1610 \u1606 ",\
+    "invalid_icon_type": "\uc0\u1575 \u1604 \u1571 \u1604 \u1608 \u1575 \u1606  \u1575 \u1604 \u1578 \u1575 \u1604 \u1610 \u1577  \u1601 \u1610  \u1578 \u1603 \u1608 \u1610 \u1606 \u1603  \u1594 \u1610 \u1585  \u1589 \u1575 \u1604 \u1581 \u1577 :"\
+  \},\
+  "card": \{\
+    "moon_phase": "\uc0\u1591 \u1608 \u1585  \u1575 \u1604 \u1602 \u1605 \u1585 ",\
+    "moon_rise": "\uc0\u1588 \u1585 \u1608 \u1602  \u1575 \u1604 \u1602 \u1605 \u1585 ",\
+    "moon_set": "\uc0\u1594 \u1585 \u1608 \u1576  \u1575 \u1604 \u1602 \u1605 \u1585 "\
+  \},\
+  "moon": \{\
+    "new_moon": "\uc0\u1575 \u1604 \u1607 \u1604 \u1575 \u1604  \u1575 \u1604 \u1580 \u1583 \u1610 \u1583 ",\
+    "waxing_crescent": "\uc0\u1607 \u1604 \u1575 \u1604  \u1605 \u1578 \u1586 \u1575 \u1610 \u1583 ",\
+    "first_quarter": "\uc0\u1575 \u1604 \u1585 \u1576 \u1593  \u1575 \u1604 \u1571 \u1608 \u1604 ",\
+    "waxing_gibbous": "\uc0\u1602 \u1605 \u1585  \u1605 \u1578 \u1590 \u1582 \u1605 ",\
+    "full_moon": "\uc0\u1576 \u1583 \u1585 ",\
+    "waning_gibbous": "\uc0\u1602 \u1605 \u1585  \u1605 \u1578 \u1606 \u1575 \u1602 \u1589 ",\
+    "last_quarter": "\uc0\u1575 \u1604 \u1585 \u1576 \u1593  \u1575 \u1604 \u1571 \u1582 \u1610 \u1585 ",\
+    "waning_crescent": "\uc0\u1607 \u1604 \u1575 \u1604  \u1605 \u1578 \u1606 \u1575 \u1602 \u1589 "\
+  \}\
+\}\
+}
